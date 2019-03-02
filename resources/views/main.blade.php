0a8b534be120a57ef7cdf895168b189912dcce30
Choo <chooweisung@gmail.com>
2019-03-03 01:10:08 +0800

    main page design

---
+++ resources/views/main.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Sunday</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('/css/main.css') }}">
+    <script src="main.js"></script>
+</head>
+<body class="body">
+    <span>
+        <a href="#image"> 
+           <img src="{{url('../image/Logo.png')}}" class="logo">
+        </a>
+    <div class="top_nav">
+    <a href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+</span>
+</body>
+</html>
